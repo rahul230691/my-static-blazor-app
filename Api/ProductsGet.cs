2863beeb08449c7e873ede40a96e4b93f2edc70b
rahul.saraswat <rahul.saraswat@inspopindia.com>
2021-02-11 22:08:55 +0530

    Add ProductsGet Api

---
+++ Api/ProductsGet.cs
@@ -0,0 +1,31 @@
+﻿using Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api
+{
+     public class ProductsGet
+    {
+        private readonly IProductData _productData;
+
+        public ProductsGet(IProductData productData)
+        {
+            _productData = productData;
+        }
+
+        [FunctionName("ProductsGet")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "products")] HttpRequest req, ILogger logger)
+        {
+            var products = _productData.GetProducts();
+            return new OkObjectResult(products);
+        } 
+    }
+}
